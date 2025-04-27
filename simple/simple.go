46e5d63d2e0978328a7ce3211b5920baba7b21d1
Fahril Hadi <fahrilhadi77@gmail.com>
2025-04-27 17:53:47 +0700

    injector parameter

--- simple/simple.go
+++ simple/simple.go
@@ -6,9 +6,9 @@ type SimpleRepository struct {
 	Error bool
 }
 
-func NewSimpleRepository() *SimpleRepository {
+func NewSimpleRepository(isError bool) *SimpleRepository {
 	return &SimpleRepository{
-		Error: true,
+		Error: isError,
 	}
 }
 
@@ -16,12 +16,12 @@ type SimpleService struct {
 	*SimpleRepository
 }
 
-func NewSimpleService(repository SimpleRepository) (*SimpleService, error) {
+func NewSimpleService(repository *SimpleRepository) (*SimpleService, error) {
 	if repository.Error {
 		return nil, errors.New("failed create service")
 	} else {
 		return &SimpleService{
-			SimpleRepository: &repository,
+			SimpleRepository: repository,
 		}, nil
 	}
 }
